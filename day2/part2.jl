5b1e776dd35f5155623709c45ea1d5ef0cd32ce9
erikbackman <erikbackman@users.noreply.github.com>
2022-12-02 18:09:36 +0100

    golfing

--- day2/part2.jl
+++ day2/part2.jl
@@ -1,4 +1,3 @@
-A = [3 4 8 1 5 9 2 6 7]
 X = zeros(Int32, 9)
 foreach(v -> X[(Int(v[1]) - Int('A'))*3 + Int(v[3]) - Int('X') + 1]+=1, readlines("input.txt"))
-println((A*X)[1])
+println(([3 4 8 1 5 9 2 6 7]*X)[1])

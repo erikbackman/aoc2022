3c9f6de0647b3996ec727f7a7cadb0de45a05ec1
erikbackman <erikbackman@users.noreply.github.com>
2022-12-02 17:48:13 +0100

    cleanup

--- day2/part2.jl
+++ day2/part2.jl
@@ -1,5 +1,4 @@
-lines=readlines("input2.txt")
 A = [3 4 8 1 5 9 2 6 7]
 X = zeros(Int32, 9)
-foreach(v -> X[(Int(v[1]) - Int('A'))*3 + Int(v[3]) - Int('X') + 1]+=1, lines)
+foreach(v -> X[(Int(v[1]) - Int('A'))*3 + Int(v[3]) - Int('X') + 1]+=1, readlines("input.txt"))
 println((A*X)[1])

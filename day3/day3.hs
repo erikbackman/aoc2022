2fed6bcbb0e06963d712662ba5fad867f3bea5af
erikbackman <erikbackman@users.noreply.github.com>
2022-12-03 16:36:01 +0100

    day3 haskell

--- day3/day3.hs
+++ day3/day3.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.List
 import Data.Monoid
 import Control.Monad
+import Control.Arrow
 
 prio :: Char -> Int
 prio c | n >= 97 && n <= 122 = (n - fromEnum 'a') + 1
@@ -15,11 +16,9 @@ part1 s = getSum . foldMap (Sum . prio) $ (nub . dupes) =<< lines s
           where dupes s = let (c1,c2) = splitAt ((length s) `div` 2) s 
                           in intersect c1 c2
 
-groupsOf :: Int -> [a] -> [[a]]
-groupsOf n = takeWhile (not . null) . unfoldr (Just . splitAt n)
-
 part2 :: String -> Int
 part2 = getSum . foldMap (Sum . prio . head . common) . groupsOf 3 . lines
         where common = foldr1 intersect
+              groupsOf n = takeWhile (not . null) . unfoldr (Just . splitAt n)
 
-main = interact (\inp -> show (part1 inp, part2 inp))
+main = interact (show . (part1 &&& part2))

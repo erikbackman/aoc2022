67b5c7e6a0a4c2ea734d2aedeca659a8d4a8ac9a
erikbackman <erikbackman@users.noreply.github.com>
2022-12-03 15:35:25 +0100

    day3 haskell

--- day3/day3.hs
+++ day3/day3.hs
@@ -15,15 +15,11 @@ part1 s = getSum . foldMap (Sum . prio) $ (nub . dupes) =<< lines s
           where dupes s = let (c1,c2) = splitAt ((length s) `div` 2) s 
                           in intersect c1 c2
 
-windowOf :: Int -> [a] -> [[a]]
-windowOf n xs
-    | len == n   = [xs]
-    | len > n   = let (w,rest) = splitAt n xs in w : windowOf n rest
-    | otherwise = []
-    where len = length xs
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf n = takeWhile (not . null) . unfoldr (Just . splitAt n)
 
 part2 :: String -> Int
-part2 = getSum . foldMap (Sum . prio . head . common) . windowOf 3 . lines
+part2 = getSum . foldMap (Sum . prio . head . common) . groupsOf 3 . lines
         where common = foldr1 intersect
 
 main = interact (\inp -> show (part1 inp, part2 inp))

c3549b07a54253719dc3c47c6afcfe9f69be40c8
erikbackman <erikbackman@users.noreply.github.com>
2022-12-04 14:57:14 +0100

    part2 julia

---
+++ day4/part2.jl
@@ -0,0 +1,11 @@
+input = readlines("input.txt")
+re = r"(?<a>\d+)-(?<b>\d+),(?<c>\d+)-(?<d>\d+)"
+
+part2 = mapreduce(line ->
+    let
+        a,b,c,d = map(s -> parse(Int, s), match(re, line));
+        i1 = [a:b;];
+        i2 = [c:d;];
+        return !iszero(intersect(i1, i2))
+    end
+    ,+,input)

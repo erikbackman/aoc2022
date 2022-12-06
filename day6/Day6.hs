77d28b0c1d2ce40228df8fb85f212e4a52d3d172
erikbackman <erikbackman@users.noreply.github.com>
2022-12-06 12:40:48 +0100

    day6 cleanup

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -2,7 +2,11 @@ module Day6 where
 
 import Data.List
 import qualified Data.Set as S
+import Control.Arrow ((&&&))
 
-solve :: Int -> String -> Maybe Int
-solve len inp = (+len) <$> findIndex ((== len) . length . S.fromList) (windows len inp)
+findIx :: Int -> String -> Maybe Int
+findIx len inp = (+len) <$> findIndex ((== len) . length . S.fromList) (windows len inp)
     where windows n = foldr (zipWith (:)) (repeat []) . take n . tails
+
+day6 = (findIx 4 &&& findIx 14)
+
